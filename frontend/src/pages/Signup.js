cbd3b8ff2767f7c4721a0d9cb2332f69ec3e77c9
Ibrahima Drame <i.d.niakhate@gmail.com>
2023-01-23 16:05:36 +0100

    update signup

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,5 +1,18 @@
 const Signup = () => {
-  return <div>sign up page</div>;
+  return (
+    <div className="signup">
+      <h3>Créer votre compte et bénéficiez de nos avantages adhérents</h3>
+      <form>
+        <div className="signupInput">
+          <input type="text" placeholder="nom" required />
+          <input type="text" placeholder="prénom" required />
+          <input type="email" placeholder="email" required />
+          <input type="password" placeholder="mot de passe" required />
+        </div>
+        <button>Créer un compte</button>
+      </form>
+    </div>
+  );
 };
 
 export default Signup;
