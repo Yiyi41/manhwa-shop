59a34b347f2b604ef95db8d044455f6f9951e8d5
Yiyi <loveloveyy@hotmail.com>
2024-01-08 16:34:31 +0100

    delet useless comments

--- backend/dbConfig.js
+++ backend/dbConfig.js
@@ -1,18 +1,3 @@
-// require("dotenv").config();
-// const { createPool } = require("promise-mysql");
-
-// const connectionOptions = {
-//   host: process.env.host,
-//   user: process.env.user,
-//   password: process.env.password,
-//   database: process.env.database,
-//   port: process.env.port
-// };
-
-// const connection = createPool(connectionOptions);
-
-// module.exports = { connection }; // es5
-
 import { createPool } from "promise-mysql";
 import dotenv from "dotenv";
 
